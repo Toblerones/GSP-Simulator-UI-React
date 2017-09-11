43640ca39b529786cd6e7bf3845f5be0e8b2097b
toblerones <toblerones@hotmail.com>
2017-09-12 01:02:54 +1000

    Add bootstrap css

--- gsp-simulator-ui/src/index.js
+++ gsp-simulator-ui/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'tether';
 
 ReactDOM.render(<App/>, document.getElementById('root'));

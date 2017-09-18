bd772371570272ea05dda6115d02e2c046cb3b7a
Toblers <toblerones.j.x.luo@hsbc.com.au>
2017-09-18 18:00:53 +1000

    update title

--- gsp-simulator/src/components/GspSimulator.js
+++ gsp-simulator/src/components/GspSimulator.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link } from 'react-router-dom'
+import {NavLink } from 'react-router-dom'
 import logo from '../images/logo.svg';
 import './GspSimulator.css';
 
@@ -9,13 +9,13 @@ export class GspSimulator extends Component {
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
+                    <h2>GSP Simulator</h2>
                 </div>
                 <div >
-                    <Link to ={'/UserKyc'}  activeClassName="active">Go KycForm</Link>
+                    <NavLink to ={'/UserKyc'} activeClassName="active">Go KycForm</NavLink>
                 </div>
                 <div >
-                    <Link to ={'/UserShortForm'}  activeClassName="active">Go ShortForm</Link>
+                    <NavLink to ={'/UserShortForm'}  activeClassName="active">Go ShortForm</NavLink>
                 </div>
             </div>
         );
